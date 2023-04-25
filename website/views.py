f6129b55e2037e8840afe50d15dc3e6948eb2913
krishnakumar b <79183768+krishnakumarbhat@users.noreply.github.com>
2023-04-25 18:45:17 +0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,42 +1,3 @@
-# from flask import Blueprint, render_template, request, flash, jsonify
-# from flask_login import login_required, current_user
-# from .models import Note
-# from . import db
-# import json
-
-# views = Blueprint('views', __name__)
-
-
-# @views.route('/', methods=['GET', 'POST'])
-# @login_required
-# def home():
-#     if request.method == 'POST': 
-#         note = request.form.get('note')#Gets the note from the HTML 
-
-#         if len(note) < 1:
-#             flash('Note is too short!', category='error') 
-#         else:
-#             new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-#             db.session.add(new_note) #adding the note to the database 
-#             db.session.commit()
-#             flash('Note added!', category='success')
-
-#     return render_template("home.html", user=current_user)
-
-
-# @views.route('/delete-note', methods=['POST'])
-# def delete_note():  
-#     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
-
-#     return jsonify({})
-
-
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
@@ -80,20 +41,22 @@ def home():
             if line.startswith('ATOM'):
                 residue_name = line[17:20].strip()
                 if residue_name == 'A':
-                    line = line[:17] + ' DA ' + line[21:]
+                    line = line[:77] + 'C' + line[78:80] + ' ' + line[81:87] + ' ' + line[88:]
                 elif residue_name == 'C':
-                    line = line[:17] + ' DC ' + line[21:]
+                    line = line[:77] + 'G' + line[78:80] + ' ' + line[81:87] + ' ' + line[88:]
                 elif residue_name == 'G':
-                    line = line[:17] + ' DG ' + line[21:]
+                    line = line[:77] + 'C' + line[78:80] + ' ' + line[81:87] + ' ' + line[88:]
                 elif residue_name == 'U':
-                    line = line[:17] + ' DT ' + line[21:]
+                    line = line[:77] + 'T' + line[78:80] + ' ' + line[81:87] + ' ' + line[88:]
+            if line.endswith(' O  \n'):
+                line = line[:77] + '  \n'
             new_file_contents += line
 
-        new_file_name = 'converted_' + file.filename
-        with open(new_file_name, 'w') as f:
-            f.writelines(new_file_contents)
+        # Save new file to server
+        with open(file.filename.split('.')[0] + '_dna.pdb', 'w') as f:
+            f.write(new_file_contents)
 
-        # Return download link to user
-        return render_template("home.html", user=current_user, download_link=new_file_name)
+        flash('File converted successfully!', category='success')
+        return render_template("home.html", user=current_user)
 
     return render_template("home.html", user=current_user)
